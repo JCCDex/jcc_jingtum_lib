a44715a05a41fb39b964fb3aa6f4b5b5f538ef07
GinMu <gin.musheng@gmail.com>
2018-09-25 15:49:42 +0800

    refactor local_sign.js

--- src/local_sign.js
+++ src/local_sign.js
@@ -1,13 +1,14 @@
-const base = require('jingtum-base-lib').Wallet;
+const base = require('jcc_jingtum_base_lib').Wallet;
 var jser = require('../lib/Serializer').Serializer;
 
-function jingtum_sign_tx(in_tx, in_v) {
-    var wt = new base(in_v.seed);
+function jingtum_sign_tx(in_tx, in_v, token) {
+    token = token || 'swt';
+    var wt = new base(in_v.seed, token);
     in_tx.SigningPubKey = wt.getPublicKey();
     var prefix = 0x53545800;
-    var hash = jser.from_json(in_tx).hash(prefix);
+    var hash = jser.from_json(in_tx, token).hash(prefix, token);
     in_tx.TxnSignature = wt.signTx(hash);
-    return jser.from_json(in_tx).to_hex();
+    return jser.from_json(in_tx, token).to_hex();
 }
 
 
