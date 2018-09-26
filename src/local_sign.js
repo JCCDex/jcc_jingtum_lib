61f24c001cd865612a0b58769dd420925fa73d79
GinMu <gin.musheng@gmail.com>
2018-09-26 16:29:21 +0800

    fixed eslint error of local_sign.js

--- src/local_sign.js
+++ src/local_sign.js
@@ -1,9 +1,9 @@
-var base = require('jcc_jingtum_base_lib').Wallet;
+var Wallet = require('jcc_jingtum_base_lib').Wallet;
 var jser = require('../lib/Serializer').Serializer;
 
 function jingtum_sign_tx(in_tx, in_v, token) {
     token = token || 'swt';
-    var wt = new base(in_v.seed, token);
+    var wt = new Wallet(in_v.seed, token);
     in_tx.SigningPubKey = wt.getPublicKey();
     var prefix = 0x53545800;
     var hash = jser.from_json(in_tx, token).hash(prefix, token);
