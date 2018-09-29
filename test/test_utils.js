6f2e7a2f71794cf63c7b83b9d436069c323e36df
GinMu <gin.musheng@gmail.com>
2018-09-29 15:36:12 +0800

    test utils example

--- test/test_utils.js
+++ test/test_utils.js
@@ -1,12 +1,428 @@
 const chai = require('chai');
 const expect = chai.expect;
-
+const config = require('./config')
+let utils = require('../src/utils');
+let txData = require('./tx_data');
+const fs = require('fs')
 describe('test remote', function () {
-    let utils = require('../src/utils');
+
+    describe('test hexToString', function () {
+        it('return "	" if the str is 09', function () {
+            let res = utils.hexToString("09");
+            expect(res).to.equal("	")
+        })
+
+        it('return "	" if the str is 09111', function () {
+            let res = utils.hexToString("17111");
+            expect(res).to.equal("\u0001q\u0011")
+        })
+    })
+
+    describe('test stringToHex', function () {
+        it('return 09 if the str is tab', function () {
+            let res = utils.stringToHex("	");
+            expect(res).to.equal('09')
+        })
+
+        it('return 31 if the str is 1', function () {
+            let res = utils.stringToHex('1');
+            expect(res).to.equal('31')
+        })
+    })
+
+    describe('test isValidAmount', function () {
+        it('return false if the amount is null', function () {
+            let valid = utils.isValidAmount(null);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the amount is undefined', function () {
+            let valid = utils.isValidAmount(undefined);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the amount is ""', function () {
+            let valid = utils.isValidAmount("");
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the amount is empty object', function () {
+            let valid = utils.isValidAmount({});
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the value is not number', function () {
+            let valid = utils.isValidAmount({
+                value: 'ststt'
+            });
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is invalid', function () {
+            let valid = utils.isValidAmount({
+                value: '0',
+                currency: 'sw'
+            });
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is swt and the issuer is not empty string', function () {
+            let valid = utils.isValidAmount({
+                value: '0',
+                currency: 'SWT',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or'
+            }, 'swt');
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is not swt and the issuer is invalid', function () {
+            let valid = utils.isValidAmount({
+                value: '0',
+                currency: 'JMOAC',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9o'
+            }, 'swt');
+            expect(valid).to.equal(false);
+        })
+
+        it('return true if the parameter is valid', function () {
+            let valid = utils.isValidAmount({
+                value: '0',
+                currency: 'JMOAC',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or'
+            }, 'swt');
+            expect(valid).to.equal(true);
+        })
+    })
+
+    describe('test isValidAmount0', function () {
+        it('return false if the amount is null', function () {
+            let valid = utils.isValidAmount0(null);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the amount is undefined', function () {
+            let valid = utils.isValidAmount0(undefined);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the amount is ""', function () {
+            let valid = utils.isValidAmount0("");
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the amount is empty object', function () {
+            let valid = utils.isValidAmount0({});
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is invalid', function () {
+            let valid = utils.isValidAmount({
+                currency: 'sw'
+            });
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is swt and the issuer is not empty string', function () {
+            let valid = utils.isValidAmount0({
+                currency: 'SWT',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or'
+            }, 'swt');
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is not swt and the issuer is invalid', function () {
+            let valid = utils.isValidAmount0({
+                currency: 'JMOAC',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9o'
+            }, 'swt');
+            expect(valid).to.equal(false);
+        })
+
+        it('return true if the parameter is valid', function () {
+            let valid = utils.isValidAmount0({
+                currency: 'JMOAC',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or'
+            }, 'swt');
+            expect(valid).to.equal(true);
+        })
+    })
+
+    describe('test parseAmount', function () {
+        it('return object if the amount is string', function () {
+            let amout = utils.parseAmount("1000000", 'swt');
+            expect(amout).to.deep.equal({
+                value: '1',
+                currency: 'SWT',
+                issuer: ''
+            })
+        })
+
+        it('return object if the amount is object and valid', function () {
+            let amout = utils.parseAmount({
+                value: '100',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                currency: 'JMOAC'
+            }, 'swt');
+            expect(amout).to.deep.equal({
+                value: '100',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                currency: 'JMOAC'
+            })
+        })
+
+        it('return null if the amount is invalid', function () {
+            let amout = utils.parseAmount({}, 'swt');
+            expect(amout).to.equal(null)
+        })
+    })
+
+    describe('test isValidCurrency', function () {
+        it('return false if the currency is null', function () {
+            let valid = utils.isValidCurrency(null);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is ""', function () {
+            let valid = utils.isValidCurrency("");
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is undefined', function () {
+            let valid = utils.isValidCurrency(undefined);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the currency is not string', function () {
+            let valid = utils.isValidCurrency({});
+            expect(valid).to.equal(false);
+        })
+
+        it('return true if the currency is swt', function () {
+            let valid = utils.isValidCurrency('swt');
+            expect(valid).to.equal(true);
+        })
+    })
+
+    describe('test isValidHash', function () {
+        it('return false if the hash is null', function () {
+            let valid = utils.isValidHash(null);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the hash is ""', function () {
+            let valid = utils.isValidHash("");
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the hash is undefined', function () {
+            let valid = utils.isValidHash(undefined);
+            expect(valid).to.equal(false);
+        })
+
+        it('return false if the hash is not string', function () {
+            let valid = utils.isValidHash({});
+            expect(valid).to.equal(false);
+        })
+
+        it('return true if the hash is valid', function () {
+            let valid = utils.isValidHash(config.testCreateHash);
+            expect(valid).to.equal(true);
+        })
+    })
+
+    describe('test arraySet', function () {
+        it('return correct value', function () {
+            let res = utils.arraySet(8, 0)
+            expect(res).to.deep.equal([0, 0, 0, 0, 0, 0, 0, 0])
+        })
+    })
+
     describe('test getCurrency', function () {
-        it('return swt if the config is empty', function () {
+        it('return default value if the config is empty', function () {
             let currency = utils.getCurrency('moac');
             expect(currency).to.equal('SWT');
+        })
+
+        it('return SWT if the token is swt', function () {
+            let currency = utils.getCurrency('swt');
+            expect(currency).to.equal('SWT');
+        })
+
+        it('return BWT if the token is bwt', function () {
+            let currency = utils.getCurrency('bwt');
+            expect(currency).to.equal('BWT');
+        })
+    })
+
+    describe('test getFee', function () {
+        it('return default value if the config is empty', function () {
+            let fee = utils.getFee('moac');
+            expect(fee).to.equal(10000);
+        })
+
+        it('return 10 if the token is bwt', function () {
+            let fee = utils.getFee('bwt');
+            expect(fee).to.equal(10);
+        })
+
+        it('return 10000 if the token is swt', function () {
+            let fee = utils.getFee('swt');
+            expect(fee).to.equal(10000);
+        })
+    })
+
+    describe('test getAccountZero', function () {
+        it('return default value if the config is empty', function () {
+            let fee = utils.getAccountZero('moac');
+            expect(fee).to.equal('jjjjjjjjjjjjjjjjjjjjjhoLvTp');
+        })
+
+        it('return bbbbbbbbbbbbbbbbbbbbbhoLvTp if the token is bwt', function () {
+            let fee = utils.getAccountZero('bwt');
+            expect(fee).to.equal('bbbbbbbbbbbbbbbbbbbbbhoLvTp');
+        })
+
+        it('return jjjjjjjjjjjjjjjjjjjjjhoLvTp if the token is swt', function () {
+            let fee = utils.getAccountZero('swt');
+            expect(fee).to.equal('jjjjjjjjjjjjjjjjjjjjjhoLvTp');
+        })
+    })
+
+    describe('test getAccountOne', function () {
+        it('return default value if the config is empty', function () {
+            let fee = utils.getAccountOne('moac');
+            expect(fee).to.equal('jjjjjjjjjjjjjjjjjjjjBZbvri');
+        })
+
+        it('return bbbbbbbbbbbbbbbbbbbbBZjvri if the token is bwt', function () {
+            let fee = utils.getAccountOne('bwt');
+            expect(fee).to.equal('bbbbbbbbbbbbbbbbbbbbBZjvri');
+        })
+
+        it('return jjjjjjjjjjjjjjjjjjjjBZbvri if the token is swt', function () {
+            let fee = utils.getAccountOne('swt');
+            expect(fee).to.equal('jjjjjjjjjjjjjjjjjjjjBZbvri');
+        })
+    })
+
+    describe('test affectedAccounts', function () {})
+
+    describe('test affectedBooks', function () {})
+
+    describe('test processTx', function () {
+        it('check sent', function () {
+            let res = utils.processTx(txData.input1, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output1)
+        })
+
+        it('check convert', function () {
+            let res = utils.processTx(txData.input2, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output2)
+        })
+
+        it('check received', function () {
+            let res = utils.processTx(txData.input3, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output3)
+        })
+
+        it('check trusted', function () {
+            let res = utils.processTx(txData.input4, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output4)
+        })
+
+        it('check trusting', function () {
+            let res = utils.processTx(txData.input5, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output5)
+        })
+
+        it('check offercancel', function () {
+            let res = utils.processTx(txData.input6, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output6)
+        })
+
+        it('check offernew: buy', function () {
+            let res = utils.processTx(txData.input7, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output7)
+        })
+
+        it('check offernew: sell', function () {
+            let res = utils.processTx(txData.input8, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output8)
+        })
+
+        it('check relationset: freeze', function () {
+            let res = utils.processTx(txData.input9, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output9)
+        })
+
+        it('check relationset: authorize', function () {
+            let res = utils.processTx(txData.input10, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output10)
+        })
+
+        it('check relationdel: unfreeze', function () {
+            let res = utils.processTx(txData.input11, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output11)
+        })
+
+        it('check relationset: unknown', function () {
+            let res = utils.processTx(txData.input12, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output12)
+        })
+
+        it('check unknown', function () {
+            let res = utils.processTx(txData.input13, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output13)
+        })
+
+        it('check configcontract: deploy', function () {
+            let res = utils.processTx(txData.input14, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output14)
+        })
+
+        it('check configcontract: call', function () {
+            let res = utils.processTx(txData.input15, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output15)
+        })
+
+        it('check configcontract: method is not 0 or 1', function () {
+            let res = utils.processTx(txData.input16, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output16)
+        })
+
+        it('check offereffect: effect is offer_partially_funded', function () {
+            let res = utils.processTx(txData.input17, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output17)
+        })
+
+        it('check offereffect: effect is offer_created', function () {
+            let res = utils.processTx(txData.input18, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output18)
+        })
+
+        it('check offereffect: effect is offer_funded', function () {
+            let res = utils.processTx(txData.input19, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output19)
+        })
+
+        it('check offereffect: effect is offer_cancelled', function () {
+            let res = utils.processTx(txData.input20, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output20)
+        })
+
+        it('check offereffect: effect is offer_cancelled and type is offercancel', function () {
+            let res = utils.processTx(txData.input21, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output21)
+        })
+
+        it('check offereffect: effect is offer_bought', function () {
+            let res = utils.processTx(txData.input22, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output22)
+        })
+
+        it('check offereffect: effect is set_regular_key', function () {
+            let res = utils.processTx(txData.input23, config.testAddress, 'swt');
+            expect(res).to.deep.equal(txData.output23)
         })
     })
 });
