50cb018941fd9a8fbcfe93ea82f40c4797dc7a40
GinMu <gin.musheng@gmail.com>
2018-09-30 14:34:50 +0800

    added test_orderbook.js

---
+++ test/test_orderbook.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const expect = chai.expect;
+const OrderBook = require('../src/orderbook');
+const Remote = require('../src/remote');
+const config = require('./config');
+const txData = require('./tx_data');
+const sinon = require('sinon');
+let {
+    JT_NODE
+} = config
+
+describe('test TypesUtils', function () {
+    describe('test constructor', function () {
+        let remote = new Remote({
+            server: JT_NODE,
+            local_sign: true
+        });
+        remote._token = null;
+        let orderBook = new OrderBook(remote);
+        expect(orderBook._token).to.equal('swt');
+    })
+
+    describe('emit transactions with meta data', function () {
+        let remote = new Remote({
+            server: JT_NODE,
+            local_sign: true
+        });
+        let orderBook = new OrderBook(remote);
+        orderBook._remote.emit('transactions', txData.input26);
+    })
+
+    describe('emit transactions without meta data', function () {
+        let remote = new Remote({
+            server: JT_NODE,
+            local_sign: true
+        });
+        let orderBook = new OrderBook(remote);
+        orderBook._remote.emit('transactions', txData.input24)
+    })
+});
