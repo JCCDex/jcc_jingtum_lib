29e28250f19e35808f9625935b45df12b47e7e7f
GinMu <gin.musheng@gmail.com>
2020-02-14 17:05:52 +0800

    support "SEAAPS" chain

--- src/config.js
+++ src/config.js
@@ -2,15 +2,18 @@
  * Created by yalei on 2017/4/25.
  */
 var config = [{
-    currency: 'SWT',
-    ACCOUNT_ZERO: 'jjjjjjjjjjjjjjjjjjjjjhoLvTp',
-    ACCOUNT_ONE: 'jjjjjjjjjjjjjjjjjjjjBZbvri',
-    fee: 10000
+  currency: 'SWT',
+  ACCOUNT_ZERO: 'jjjjjjjjjjjjjjjjjjjjjhoLvTp',
+  ACCOUNT_ONE: 'jjjjjjjjjjjjjjjjjjjjBZbvri',
+  fee: 10000
 }, {
-    currency: 'BWT',
-    ACCOUNT_ZERO: 'bbbbbbbbbbbbbbbbbbbbbhoLvTp',
-    ACCOUNT_ONE: 'bbbbbbbbbbbbbbbbbbbbBZjvri',
-    fee: 10
+  currency: 'BWT',
+  ACCOUNT_ZERO: 'bbbbbbbbbbbbbbbbbbbbbhoLvTp',
+  ACCOUNT_ONE: 'bbbbbbbbbbbbbbbbbbbbBZjvri',
+  fee: 10
+}, {
+  currency: 'SEAA',
+  fee: 10000
 }];
 
 module.exports = config;
